dbbdc4adba5898d33e0d574bbfa0b8c13783dd2c
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2026-07-02 17:19:41 +0530

    Time: 1 ms (99.19%), Space: 42.8 MB (45.92%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,16 +1,13 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        Map<Integer,Integer>map = new HashMap<>();
-        int num = n;
-        while(num>0){
-            int l = num%10;
-            map.put(l,map.getOrDefault(l,0)+1);
-            num/=10;
-        }
-        int res= 0;
-        for(int k :map.keySet()){
-            res+= k * map.get(k);
-        }
-        return res;
+        int sum = 0;
+        int rem = 0;
+       while(n>0)
+       {
+        rem = n % 10;
+        sum +=rem;
+        n /=10;
+       } 
+       return sum;
     }
 }
