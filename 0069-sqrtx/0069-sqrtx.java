30879fa636a6d5ceb5b1c2c4a86f49ea530fc64f
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2026-07-09 23:43:40 +0530

    Time: 1 ms (97.64%), Space: 42.3 MB (89.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,28 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        // Base case: handling 0 and 1 explicitly avoids division by zero
-        if (x < 2) {
+        if(x<2){
             return x;
         }
-
         int low = 1;
         int high = x;
-        int res = 0;
-        
-        while (low <= high) {
-            // Fix 1: Prevent overflow when calculating mid
+        int res =0;
+        while(low<=high){
             int mid = low + (high - low) / 2;
-            
-            // Fix 2: Use division instead of multiplication (mid * mid == x)
-            if (mid == x / mid) {
+            if(mid == x/mid){
                 return mid;
             }
-            
-            if (mid < x / mid) {
-                low = mid + 1;
-                res = mid;
-            } else {
-                high = mid - 1;
+            if(mid<x/mid){
+                low = mid+1;
+                res=mid;
+            }
+            else{
+                high = mid-1;
             }
         }
         return res;
