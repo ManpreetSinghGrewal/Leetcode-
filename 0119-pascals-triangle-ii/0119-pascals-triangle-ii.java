173940b1dea138dea5e2871dd82ddcc295b9a58d
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2026-07-24 16:20:37 +0530

    Time: 1 ms (70.05%), Space: 42.1 MB (96.7%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,23 +1,24 @@
 class Solution {
-    Map<String,Integer>map  = new HashMap <>();
-    public int helper(int row, int col){
-         if(row == 0|| col ==0|| row == col){
-            return 1;
-         }
-         String key = Integer.toString(row) + "" + Integer.toString(col);
-         if(map.containsKey(key)){
-            return map.get(key);
-         }
-        int res = helper(row-1,col-1)+ helper(row-1,col);
-        map.put(key,res);
-        return map.get(key);
-    }
     public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer>res = new ArrayList<>();
-        int row = rowIndex;
-        for(int col = 0;col<=rowIndex;col++){
-            res.add(helper(row,col));
+        
+        List<Integer> row = new ArrayList<>();
+
+        row.add(1);
+
+        for(int i=1; i<= rowIndex; i++){
+
+            for(int j=row.size()-1; j>=1; j--){
+
+                row.set(j, row.get(j) + row.get(j-1));
+
+                //start addition from right to left
+                //we are not creating new list everytime, we are overwriting the numbers after adding on the same list for every new row, so we have to take care that we don't overwrite numbers before we are finished using them
+
+            }
+
+        row.add(1);
         }
-        return res;
+
+        return row;
     }
 }
