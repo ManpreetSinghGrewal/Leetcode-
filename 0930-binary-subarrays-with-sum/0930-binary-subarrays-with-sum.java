2e5144c9f4872987d78afc941a4f2d2448a3f0df
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2026-08-06 01:46:19 +0530

    Time: 23 ms (33.21%), Space: 52.8 MB (14.33%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int result = 0;
+        int prefixsum =0;
+
+         for(int num : nums){
+            prefixsum += num;
+            if(map.containsKey(prefixsum - k)){
+                result += map.get(prefixsum -k);
+            }
+
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+         }
+         return result;
+        
+    }
+}
