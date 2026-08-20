39afe721e31bdd6d5e07bd06dc42942df9efd66d
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2026-08-20 12:02:58 +0530

    Time: 4 ms (99.07%), Space: 47.8 MB (29.18%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean phelper(int i,int j,String s){
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+         int i = 0;
+         int j = s.length()-1;
+         while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return phelper(i,j-1,s) || phelper(i+1,j,s);
+            }
+            else{
+                i++;
+                j--;
+            }
+         }
+         return true;
+    }
+}
